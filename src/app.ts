8caf44962b6af72ce5ca34062a5879bc3f7b9995
Wellington José <paivawellington23@gmail.com>
2022-10-17 15:19:14 -0300

    feat: added base project

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import router from "./routes/routes";
+import { AppError } from "./errors/appError";
+
+const app = express();
+
+app.use(cors());
+
+app.use(router);
+
+app.listen(process.env.PORT || 4000);
+console.log("Online");
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode!).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+export default app;
